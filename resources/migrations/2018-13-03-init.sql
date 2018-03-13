9a6aa477e681b0ac866b540b8797edd754fe7538
Juraj Martinka <jumarko@gmail.com>
2018-03-13 15:07:13 +0100

    DB migration script - config.edn still not correct.
    I have to figure out how to do that properly - asked on Clojurians slack #duct channel:
    
    [2:36 PM]
    jumar I'm trying to setup ragtime migrations in my project - looking at https://github.com/duct-framework/migrator.ragtime. I really don't want to write my sql scripts inside the config.edn file.
    There's a mention that URLs are possible:
    ```Migrations built in this way expect an :up and a :down key that contain vectors of SQL, either as strings, or URLs to resources on the classpath.
    ```
    
    However, I'm having a hard time to figure out how to do that.
    I've tried this:
    ``` :duct.migrator/ragtime
     {:migrations [#ig/ref :my-app.migrations/init]}
    
     [:duct.migrator.ragtime/sql :my-app.migrations/init]
     {:up ["migrations/2018-13-03-init.sql"]
      :down []}
    
    ```
    but got sql exception:
    ```Caused by org.postgresql.util.PSQLException
       ERROR: syntax error at or near "migrations" Position: 1
    
    ```
    How people usually do that?
    I can't imagine that putting all those scripts into a config is a manageable way to do that...
    Ideally, I'd like to just configure the directory that contains all db migration scripts and everything else will be handled automatically without any need to always add a new config when new migration script is added.

---
+++ resources/migrations/2018-13-03-init.sql
@@ -0,0 +1,21 @@
+-- Holds all data about authenticated users
+-- see https://github.com/luminus-framework/luminus-template/blob/master/resources/leiningen/new/luminus/db/migrations/add-users-table.up.sql
+CREATE TABLE users
+(id SERIAL PRIMARY KEY,
+first_name VARCHAR(64),
+last_name VARCHAR(64),
+email VARCHAR(64),
+admin BOOLEAN,
+last_login TIMESTAMP,
+is_active BOOLEAN,
+pass VARCHAR(300));
+
+
+-- Blog posts data:
+CREATE TABLE IF NOT EXISTS posts
+(id SERIAL PRIMARY KEY,
+user_id INT REFERENCES users(id),
+title VARCHAR(256),
+text TEXT,
+created_date TIMESTAMP,
+updated_date TIMESTAMP);
